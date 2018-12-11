d0d68c1304a880d2cacf9277b79d3422a5be2cbe
ericymtai <ericymtai@Erics-MacBook-Air.local>
2018-12-11 09:07:50 -0500

    for debug

---
+++ graph.js
@@ -0,0 +1,52 @@
+
+const dims = { height: 500, width: 1100 };
+
+const svg = d3.select('.canvas')
+  .append('svg')
+  .attr('width', dims.width + 100)
+  .attr('height', dims.height + 100);
+
+const graph = svg.append('g')
+  .attr('transform', 'translate(50, 50)');
+
+// data start
+const stratify = d3.stratify()
+  .id(d => d.name)
+  .parentId(d => d.parent);
+
+
+// update function
+const update = (data) => {
+
+  // get updated root Node data
+  const rootNode = stratify(data);    //  Uncaught Error: missing: 
+
+};
+
+// data & firebase hook-up
+var data = [];
+
+db.collection('employees').onSnapshot(res=> {
+
+  res.docChanges().forEach(change => {
+
+    const doc = {...change.doc.data(), id: change.doc.id};
+
+    switch (change.type) {
+      case 'added':
+        data.push(doc);
+        break;
+      case 'modified':
+        const index = data.findIndex(item => item.id == doc.id);
+        data[index] = doc;
+        break;
+      case 'removed':
+        data = data.filter(item => item.id !== doc.id);
+        break;
+      default:
+        break;
+    }
+
+  });
+  update(data);
+});
